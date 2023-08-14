bdf0a4cb6483f6952320d1a30f035d653143c8c0
Jordan Osikowicz <124842164+JOSIK95@users.noreply.github.com>
2023-08-14 16:32:08 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import NameTagList from "./NameTagList.js";
+import UserInput from "./UserInput.js";
+
+class App extends Component {
+  state = {
+    names: []
+  };
+
+  removeName = (clickedIndex) => {
+    // to learn how the .filter method works, check out https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+    const filterCallback = (_, index) => index !== clickedIndex;
+    const newNames = this.state.names.filter(filterCallback);
+    this.setState({ names: newNames });
+  };
+
+  addName = (name) => {
+    const newNames = [name, ...this.state.names];
+    this.setState({ names: newNames });
+  };
+
+  componentDidUpdate() {
+    const savedNameString = JSON.stringify(this.state.names);
+    localStorage.setItem("savedNames", savedNameString);
+  }
+
+  componentDidMount() {
+    const savedNamesString = localStorage.getItem("savedNames");
+    if (savedNamesString) {
+      const savedNames = JSON.parse(savedNamesString);
+      this.setState({ names: savedNames });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Name Tag Generator</h1>
+        <UserInput addName={this.addName} />
+        <NameTagList names={this.state.names} removeName={this.removeName} />
+      </div>
+    );
+  }
+}
+
+export default App;
